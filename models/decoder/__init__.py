a9b26e765b540eaa5e7633a86ff2111ae8523fc2
RongYi <31407999+EasyRy@users.noreply.github.com>
2024-06-17 23:30:36 +0800

    Update __init__.py

--- models/decoder/__init__.py
+++ models/decoder/__init__.py
@@ -1,3 +1,3 @@
-from .kernel import Decoder
+from .kernel import Decoder, Decoder_s
 from .kernel_o import Decoder_o
-__all__ = ['Decoder', 'Decoder_o']
+__all__ = ['Decoder', 'Decoder_s', 'Decoder_o']
